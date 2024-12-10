3ea8a12b9bb9bbe17901ca78fd7125d9aee347a2
Fedi Koubaa <94203102+koubaafedi@users.noreply.github.com>
2024-12-10 13:24:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+
 provider "azurerm" {
   features {}
-
+  subscription_id = var.subscription_id
   use_msi = true
 }
+
 
 
 resource "azurerm_resource_group" "rg" {
@@ -10,7 +12,7 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "jenkins-terraform-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -24,7 +26,7 @@ resource "azurerm_app_service" "app_service" {
   name                = "jenkins-terraform-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
 
   site_config {
     linux_fx_version = "PYTHON|3.9"
